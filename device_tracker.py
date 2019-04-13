7c6a1263bc6182379d4294e194c2297871b95433
TassSinclair <andrew.hamilton@thoughtworks.com>
2019-04-13 19:44:08 +1000

    Remove types for Python 2

--- device_tracker.py
+++ device_tracker.py
@@ -21,7 +21,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
         voluptuous.All(cv.ensure_list, [cv.string]),
 })
 
-def get_scanner(hass, config) -> DeviceScanner:
+def get_scanner(hass, config):
     _LOGGER.warning('Attempting to get ZTE Device Scanner')
     info = config[DOMAIN]
     host = info.get(CONF_HOST)
@@ -57,7 +57,7 @@ class ZteDeviceScanner(DeviceScanner):
         self.perform_login()
         self.results = self.zte_client.get_connected_devices()
 
-    def scan_devices(self) -> List[str]:
+    def scan_devices(self):
         self.perform_device_scan
         
         devices = []
@@ -73,7 +73,7 @@ class ZteDeviceScanner(DeviceScanner):
                 devices.append(device.mac_address)
         return devices
 
-    def get_device_name(self, device) -> str:
+    def get_device_name(self, device):
         parts = device.split("_")
         mac_address = parts[0]
         ap_mac_address = None
