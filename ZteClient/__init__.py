46e991c34656859ae3822f4b0b17cf991f0f6505
TassSinclair <andrew.hamilton@thoughtworks.com>
2019-04-13 15:56:46 +1000

    Add working get_connected_devices call. :tada:

--- ZteClient/__init__.py
+++ ZteClient/__init__.py
@@ -1,15 +1,19 @@
 import requests
 import logging
+import hashlib
+from collections import namedtuple
 import xml.etree.ElementTree as ET
 
 _LOGGER = logging.getLogger(__name__)
 logging.basicConfig(filename="debug.log",level=logging.DEBUG)
 
+Device = namedtuple("Device", ["host_name", "ip_address", "ipv6_address", "mac_address"])
+get_text = lambda element: element.text
+
 class ZteClient():
-    
-    def __init__(self, host=None, port=None, user=None, password=None):
+
+    def __init__(self, password, host="10.0.0.20", user="admin"):
         self.host = host
-        self.port = port
         self.user = user
         self.password = password
         self.baseUrl = "http://{}/".format(host)
@@ -19,11 +23,10 @@ class ZteClient():
     def login(self):
         try:
             login_token = self.__get_login_token()
-            self.login_cookies = self.__post_login_and_get_cookies()
-
+            self.login_cookies = self.__post_login_and_get_cookies(login_token)
 
         except requests.exceptions.RequestException:
-            _LOGGER.exception("failed to get a login token")
+            _LOGGER.exception("failed to perform login")
 
     def __get_login_token(self):
         loginTokenUrl = self.baseUrl + "function_module/login_module/login_page/logintoken_lua.lua"
@@ -32,7 +35,37 @@ class ZteClient():
         _LOGGER.info("got login token {}".format(login_token))
         return login_token
 
-    def __post_login_and_get_cookies(self):
-        response = requests.post(self.baseUrl, timeout=30, verify=False, cookies=self.cookie_jar)
-        _LOGGER.info("got login cookies {}".format(response.cookies.items()))
-        return response.cookies
+    def __post_login_and_get_cookies(self, login_token):
+        encodedPassword = hashlib.sha256((self.password + login_token).encode('utf-8')).hexdigest()
+        data = {'action': (None, 'login'), 'Username': (None, 'admin'), 'Password': (None, encodedPassword)}
+        _LOGGER.info(data)
+        response = requests.post(self.baseUrl, timeout=30, verify=False, files=data, cookies=self.cookie_jar, allow_redirects=False)
+        cookies = response.cookies
+        self.cookie_jar.update(cookies)
+        _LOGGER.info("got login cookies {}".format(cookies.items()))
+        requests.get(self.baseUrl, timeout=30, verify=False, cookies=cookies)
+        return cookies
+
+    def get_connected_devices(self):
+        try:
+            devices = self.__get_connected_devices("wlanDevice") + \
+                self.__get_connected_devices("lanDevice")
+            return devices
+        except requests.exceptions.RequestException:
+            _LOGGER.exception("failed to get devices devices")
+
+    def __get_connected_devices(self, connection_type):
+        device_list_url = self.baseUrl + "getpage.lua"
+        params = {'pid': '1005', 'InstNum': '5', '_': '1', 'nextpage': 'home_{}_lua.lua'.format(connection_type)}
+        response = requests.get(device_list_url, timeout=30, verify=False, cookies=self.cookie_jar, params=params)
+        root = ET.fromstring(response.text)
+        instances = root.find('OBJ_ACCESSDEV_ID').findall('Instance')
+        devices = list(map(self.__instance_to_device, instances))
+        _LOGGER.info('found {} {} devices'.format(len(devices), connection_type))
+        return devices
+
+    def __instance_to_device(self, instance):
+        param_names = map(get_text, instance.findall('ParaName'))
+        param_values = map(get_text, instance.findall('ParaValue'))
+        params = dict(zip(param_names, param_values))
+        return Device(params['HostName'], params['IPAddress'], params['IPV6Address'], params['MACAddress'])
--- ZteClient/__main__.py
+++ ZteClient/__main__.py
@@ -1,8 +1,29 @@
+import sys
+from argparse import ArgumentParser
 from ZteClient import ZteClient
 
+def argparser():
+
+    parser = ArgumentParser(prog='ZteClient')
+
+    router_args = parser.add_argument_group("router connection config")
+    router_args.add_argument("--host", help="Hostname or IP address, defaults to '10.0.0.20'")
+    router_args.add_argument("--user", help="Web UI admin username account, defaults to 'admin'")
+    router_args.add_argument("--password", help="Web UI admin password")
+    
+    subparsers = parser.add_subparsers(
+            description="Runs subcommand against the specified router",
+            dest="subcommand")
+    subparsers.add_parser("get_connected_devices", help="Get all connected devices")
+
+    return parser
+
 def main():
-    zte_client = ZteClient("10.0.0.1", 80, "admin", "password")
+    args = argparser().parse_args(sys.argv[1:])
+    zte_client = ZteClient(args.password, args.host, args.user)
     zte_client.login()
+    if args.subcommand == 'get_connected_devices':
+        print(zte_client.get_connected_devices())
 
 if __name__ == '__main__':
     main()
